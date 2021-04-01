2b4ad3362ec1206089bd2b3014a9d782f012c51c
otavio <otaviosilva2632@gmail.com>
2021-04-01 19:51:40 -0300

    feat: create functions in news controller

--- app/Http/Controllers/V1/News/NewsController.php
+++ app/Http/Controllers/V1/News/NewsController.php
@@ -6,6 +6,11 @@ namespace App\Http\Controllers\V1\News;
 
 use App\Http\Controllers\AbstractController;
 use App\Services\News\NewsService;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use function Symfony\Component\Translation\t;
 
 /**
  * Class NewsController
@@ -28,5 +33,80 @@ class NewsController extends AbstractController
     public function __construct(NewsService $service)
     {
         parent::__construct($service);
+    }
+
+    /**
+     * @param Request $request
+     * @param int $author
+     * @return JsonResponse
+     */
+    public function findByAuthor(Request $request, int $author): JsonResponse
+    {
+        try {
+            $limit = (int) $request->get('limit', 10);
+            $orderBy = $request->get('order_by', []);
+
+            $result = $this->service->findByAuthor($author, $limit, $orderBy);
+            $response = $this->successResponse($result, Response::HTTP_PARTIAL_CONTENT);
+        } catch (Exception $e) {
+            $response = $this->errorResponse($e);
+        }
+
+        return response()->json($response, $response['status_code']);
+    }
+
+    /**
+     * @param Request $request
+     * @param string $param
+     * @return JsonResponse
+     */
+    public function findBy(Request $request, string $param): JsonResponse
+    {
+        try {
+            $result = $this->service->findBy($param);
+            $response = $this->successResponse($result);
+        } catch (Exception $e) {
+            $response = $this->errorResponse($e);
+        }
+
+        return response()->json($response, $response['status_code']);
+    }
+
+    /**
+     * @param Request $request
+     * @param string $param
+     * @return JsonResponse
+     */
+    public function deleteBy(Request $request, string $param): JsonResponse
+    {
+        try {
+            $result = $this->service->deleteBy($param);
+            $response = $this->successResponse([
+                'deletado' => $result
+            ]);
+        } catch (Exception $e) {
+            $response = $this->errorResponse($e);
+        }
+
+        return response()->json($response, $response['status_code']);
+    }
+
+    /**
+     * @param Request $request
+     * @param int $author
+     * @return JsonResponse
+     */
+    public function deleteByAuthor(Request $request, int $author): JsonResponse
+    {
+        try {
+            $result = $this->service->deleteByAuthor($author);
+            $response = $this->successResponse([
+                'deletado' => $result
+            ]);
+        } catch (Exception $e) {
+            $response = $this->errorResponse($e);
+        }
+
+        return response()->json($response, $response['status_code']);
     }
 }
