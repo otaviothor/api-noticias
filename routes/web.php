f52a2b9419f352e98b748f3ebc1639c548718118
otavio <otaviosilva2632@gmail.com>
2021-04-01 20:16:58 -0300

    feat: create news routes

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(['prefix' => 'api/v1', 'namespace' => 'Author'], function () use ($router) {
+$router->group(['prefix' => 'api/v1', 'namespace' => 'V1\Author'], function () use ($router) {
     $router->post('/autores', [
         'uses' => 'AuthorController@create'
     ]);
@@ -33,5 +33,32 @@ $router->group(['prefix' => 'api/v1', 'namespace' => 'Author'], function () use 
     ]);
     $router->delete('/autores/{id}', [
         'uses' => 'AuthorController@delete'
+    ]);
+});
+
+$router->group(['prefix' => 'api/v1', 'namespace' => 'V1\News'], function () use ($router) {
+    $router->post('/noticias', [
+        'uses' => 'NewsController@create'
+    ]);
+    $router->get('/noticias', [
+        'uses' => 'NewsController@findAll'
+    ]);
+    $router->get('/noticias/autor/{author}', [
+        'uses' => 'NewsController@findByAuthor'
+    ]);
+    $router->delete('/noticias/autor/{author}', [
+        'uses' => 'NewsController@deleteByAuthor'
+    ]);
+    $router->get('/noticias/{param}', [
+        'uses' => 'NewsController@findBy'
+    ]);
+    $router->put('/noticias/{param}', [
+        'uses' => 'NewsController@editBy'
+    ]);
+    $router->patch('/noticias/{param}', [
+        'uses' => 'NewsController@editBy'
+    ]);
+    $router->delete('/noticias/{param}', [
+        'uses' => 'NewsController@deleteBy'
     ]);
 });
