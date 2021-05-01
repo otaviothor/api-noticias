220ee94fde79c0f9e73fe6139cb9c504de67b000
otavio <otaviosilva2632@gmail.com>
2021-05-01 19:16:15 -0300

    feat: create helpers to treat order by route param

---
+++ app/Helpers/OrderByHelpers.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Helpers;
+
+use http\Exception\InvalidArgumentException;
+
+/**
+ * Class OrderByHelpers
+ * @package App\Helpers
+ */
+class OrderByHelpers
+{
+    public static function treatOrderBy(string $orderBy): array
+    {
+        $orderByArray = [];
+
+        foreach (explode(',', $orderBy) as $value) {
+            $value = trim($value);
+
+            if (!preg_match("/^(-)?[A-Za-z0-9_]+$/", $value)) {
+                throw new InvalidArgumentException('O parâmetro "order_by" não está em um formato válido!');
+            }
+
+            $orderByArray[$value] = 'ASC';
+
+            if (strstr($value, '-')) {
+                $orderByArray[$value] = 'DESC';
+            }
+        }
+
+        return $orderByArray;
+    }
+}
