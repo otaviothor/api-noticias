0586acbf37ace0fdf0048c48c7453e4dfea096ee
thor <otaviosilva2632@gmail.com>
2021-06-24 22:22:19 -0300

    feat: set locale for use translations

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,8 +26,8 @@ $app = new Laravel\Lumen\Application(
 );
 
 $app->withFacades();
-
 $app->withEloquent();
+app("translator")->setLocale("pt_BR");
 
 /*
 |--------------------------------------------------------------------------
